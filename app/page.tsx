d17cd99d5c9385749d926fe0883efd7a8f291c87
btahir89 <bilal.tahir89@gmail.com>
2023-10-10 21:23:30 -0700

    more pics

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,14 @@ import Twitter from '@/app/components/Twitter'
 export default function IndexPage() {
   return (
     <main className='bg-white text-slate-900 h-full w-full min-h-screen py-12 lg:px-6 relative max-w-6xl mx-auto'>
+      <img
+        src='/logo.png'
+        alt='AI GIFs'
+        className='h-10 absolute top-0 left-0 mt-4 ml-4'
+      />
       <div className='absolute top-0 right-0 mt-4 mr-4 flex items-center'>
-        <Github className='hover:text-blue-600' />
-        <Twitter className='ml-3 hover:text-blue-600' />
+        <Github className='hover:text-blue-600 h-6 w-6' />
+        <Twitter className='ml-3 hover:text-blue-600 h-6 w-6' />
       </div>
       <h1 className='text-4xl sm:text-5xl font-extrabold text-center text-blue-950'>
         AI GIFs
