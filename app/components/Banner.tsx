0a62d5c26bc08764c83fa550e4c2e7d9575a70c2
btahir89 <bilal.tahir89@gmail.com>
2024-02-06 17:00:57 -0800

    quote build fail

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -16,7 +16,7 @@ export default function Banner() {
             >
               <circle cx={1} cy={1} r={1} />
             </svg>
-            Give us one image, and we'll give you galleries of glam, grit,
+            Give us one image, and we&apos;ll give you galleries of glam, grit,
             grace, and more.&nbsp;
             <span aria-hidden='true'>&rarr;</span>
           </a>
